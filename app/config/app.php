0b55aea8aba505e24438675fb83931602e5223fa
Alasdair Smith <alasdairsmith100@gmail.com>
2014-01-06 14:20:28 +0000

    Added Pusherer library and removed official pusher library

--- app/config/app.php
+++ app/config/app.php
@@ -107,6 +107,7 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 
+		'Artdarek\Pusherer\PushererServiceProvider',
 	),
 
 	/*
---
+++ app/config/packages/artdarek/pusherer/config.php
@@ -0,0 +1,31 @@
+<?php 
+
+return array(
+	
+	/*
+	|--------------------------------------------------------------------------
+	| Pusher Config
+	|--------------------------------------------------------------------------
+	|
+	| Pusher is a simple hosted API for quickly, easily and securely adding
+	| realtime bi-directional functionality via WebSockets to web and mobile 
+	| apps, or any other Internet connected device.
+	|
+	*/
+
+	/**
+	 * App id
+	 */
+	'app_id' => '62998',
+
+	/**
+	 * App key
+	 */
+	'key' => '7b6a2e8dc8823ec90e0f',
+
+	/**
+	 * App Secret
+	 */
+	'secret' => '7e11f320dec58666af20'
+
+);
