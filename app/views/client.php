89368cdc2d3845a961bd9667a7bf618c18e04a02
Alex Reynolds <cam00674@myport.ac.uk>
2014-01-07 14:41:12 +0000

    added default stylesheet and a basic table of dummy data

--- app/views/client.php
+++ app/views/client.php
@@ -4,11 +4,29 @@
 	<meta charset="UTF-8">
 	<title>Laravel PHP Framework</title>
 
+	<link rel="stylesheet" type="text/css" href="/css/global.css">
 	<script type="text/javascript" src="http://js.pusher.com/2.1/pusher.min.js"></script>
 </head>
 <body>
 	<div class="welcome">
 		<h1>Pusher Test</h1>
+
+		<table class="fixtures" cellspacing="0">
+			<tbody>
+				<tr id="match123">
+					<td class="team home">Arsenal</td>
+					<td class="goals homeGoals">0</td>
+					<td class="goals awayGoals">1</td>
+					<td class="team away">Chelsea</td>
+				</tr>
+				<tr id="match124">
+					<td class="team home">Crystal Palace</td>
+					<td class="goals homeGoals">2</td>
+					<td class="goals awayGoals">0</td>
+					<td class="team away">Tottenham</td>
+				</tr>
+			</tbody>
+		</table>
 
 		<script>
 			var pusher = new Pusher(<?php echo '\'' . Config::get('pusherer::key') . '\'' ?>)
