86730c6287942b63b9a4e72cb5133829ca05ae35
root <root@agpreynolds.co.uk>
2014-07-09 08:45:42 -0400

    Did something on live - not sure what but I think its important

--- app/models/Fixture.php
+++ app/models/Fixture.php
@@ -75,7 +75,7 @@ class Fixture extends Eloquent {
 
 	protected static function _getOngoingWithTeam()
 	{			
-		return self::whereBetween('startTime', [date("Y-m-d H:i:s",mktime(date('H')-2)),date("Y-m-d H:i:s")])
+		return self::whereBetween('startTime', [date("Y-m-d H:i:s",mktime(date('H')-3)),date("Y-m-d H:i:s")])
 			->with('homeTeam.teamDetails')
 			->with('awayTeam.teamDetails');
 	}
